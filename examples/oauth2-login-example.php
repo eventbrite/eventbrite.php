4e855adb1506f1fe2e9b9fff270748d926fc474d
ryanjarvinen <ryan.jarvinen@gmail.com>
2011-11-18 01:30:05 -0800

    Outlining 5 easy steps to OAuth2.0 victory with Eventbrite!

--- examples/oauth2-login-example.php
+++ examples/oauth2-login-example.php
@@ -1,7 +1,7 @@
 <?php
-# Getting Started with OAuth2 for Eventbrite #
+# Getting Started with OAuth2.0 for Eventbrite in 5 easy steps #
 
-# 1. load the API Client library #
+# 1. load the API Client library:
 include "../Eventbrite.php"; 
 
 # 1a. (optional) This example uses PHP's built-in $_SESSION storage:
@@ -9,29 +9,32 @@ include "../Eventbrite.php";
 // This line may be needed to enable session support on your server:
 session_start();
 
-# 2. Set API key and other config variables
+# 2. Set your API_Key and other required config variables:
 //You can request an API_Key at http://www.eventbrite.com/api/key
 $api_key = 'YOUR_APP_KEY';  
-//Make sure to keep your "client secret" a secret.
+//Make sure to keep your "client_secret" a secret
 $client_secret = 'YOUR_CLIENT_SECRET'; 
 // To comply with our developer terms, your user's "access_tokens" 
 // should be protected, and should not be exposed to other users.
 
 # 3. Integrate this work into your controller or routing code:
 if( isset($_GET['logout']) && $_GET['logout']=="true" ){ 
+    // clear this user's access_token -
     Eventbrite::deleteAccessToken(); 
+    // remove our "logout=true" trigger from the querystring-
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
 }
-# 4. Update your API_Key's redirect_uri setting on eventbrite.com
-# 5. Create a login widget, like this:
+# 4. Update your API_Key's "redirect_uri" setting on http://eventbrite.com/api/key
+// Your redirect_uri should be set to a page on your site where this widget is accessible.
+
+# 5. Finally, create a login widget - Like this:
 ?>
 <html>
-  <?= Eventbrite::loginWidget(array(
-                  'app_key' => $api_key,
-                  'client_secret' => $client_secret));?>
+  <?= Eventbrite::loginWidget(array( 'app_key' => $api_key,
+                               'client_secret' => $client_secret));?>
 
-  <?//additional debug output:
+  <?    //additional debug output:
     if ( Eventbrite::getAccessToken()){
       print "<p>This user's OAuth2.0 access_token is: " . Eventbrite::getAccessToken() . "</p>";
     } 
